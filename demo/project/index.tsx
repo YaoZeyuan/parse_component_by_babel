14c71539696fbd5097bb66ca79954d57d2ef73ef
yaozeyuan001 <yaozeyuan001@ke.com>
2020-09-21 23:37:07 +0800

    feat: 添加测试demo

--- demo/project/index.tsx
+++ demo/project/index.tsx
@@ -1,13 +1,39 @@
 // @ts-nocheck
 import React, { Component } from 'react';
 import { Image } from 'antd';
+import { 包装器 } from 'demoProject';
+import {
+  子目录解构组件,
+  子目录解构组件v2 as 子组件别名,
+  未使用子组件,
+  导入后通过别名使用的子组件,
+} from 'demoProject/dist/子目录';
+import 子目录直接导出组件 from 'demoProject/dist/子目录';
+import 组件库别名 from 'demoProject';
+import * as 组件库汇总结果 from 'demoProject';
 
+@包装器
 export class ImageItem extends Component<any, any> {
   static defaultProps = {
     style: {},
   };
 
   render() {
-    return <Image>Hello World</Image>;
+    let a = 组件库别名();
+    let b = 组件库别名.子组件名();
+    let c = 组件库汇总结果.汇总结果下的子函数();
+    let 导入后的子组件别名 = 导入后通过别名使用的子组件;
+
+    let { 子组件标签名 } = 组件库别名;
+    return (
+      <div>
+        <导入后的子组件别名 props={123} />
+        <子组件别名 props={123} />
+        <子组件标签名 props={123} />
+        <子目录解构组件 props={123} />
+        <子目录直接导出组件 props={123} />
+        <Image>Hello World</Image>
+      </div>
+    );
   }
 }
